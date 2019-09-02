0d18f550a0536c7aab4bf007c5568d1d02469040
guqing <1484563614@qq.com>
2019-09-02 16:59:29 +0800

    Implement stacks using arrays

---
+++ src/xyz/guqing/stack/ArrayStack.java
@@ -0,0 +1,111 @@
+package xyz.guqing.stack;
+/**
+ * 使用数组实现栈
+ * @author guqing
+ *
+ */
+public class ArrayStack {
+	/**
+	 * 栈的大小
+	 */
+	private int maxSize;
+	/**
+	 * 数组模拟栈，数据存放到该int数组中
+	 */
+	private int[] stack;
+	/**
+	 * top表示栈顶，初始化为-1即栈中没有数据
+	 */
+	private int top = -1;
+	
+	public ArrayStack(int maxSize) {
+		this.maxSize = maxSize;
+		this.stack = new int[maxSize];
+	}
+	
+	/**
+	 * 判断栈是否满
+	 * @return 栈满返回true，否则返回false
+	 */
+	public boolean isFull() {
+		return top == maxSize;
+	}
+	
+	/**
+	 * 判断栈是否为空
+	 * @return 栈为空返回true，否则返回false
+	 */
+	public boolean isEmpty() {
+		return top == -1;
+	}
+	
+	/**
+	 * 压栈
+	 * @param value 需要存入栈顶的数据
+	 */
+	public void push(int value) {
+		if(isFull()) {
+			System.out.println("栈已经满了无法再压入数据");
+			return;
+		}
+		// 栈顶先挪一个位置
+		top++;
+		// 再将数据压入栈顶
+		this.stack[top] = value;
+	}
+	
+	/**
+	 * 出栈，返回栈顶元素
+	 * @return 返回栈顶元素
+	 */
+	public int pop() {
+		if(isEmpty()) {
+			throw new RuntimeException("栈为空无法取出数据");
+		}
+		// 先保存栈顶数据
+		int value = this.stack[top];
+		// 栈顶-1
+		top--;
+		return value;
+	}
+	
+	/**
+	 * 遍历栈,从栈顶开始遍历
+	 */
+	@Override
+	public String toString() {
+		if(isEmpty()) {
+			return "[]";
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("stack [");
+		for(int i = top; i >= 0; i--) {
+			sb.append(stack[i]);
+			
+			if(i != 0) {
+				sb.append(", ");
+			}
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		ArrayStack stack = new ArrayStack(5);
+		// 压栈
+		stack.push(10);
+		stack.push(5);
+		stack.push(20);
+		stack.push(8);
+		stack.push(6);
+		
+		// 打印栈
+		System.out.println(stack);
+		
+		// 弹栈
+		for(int i=4; i>=0; i--) {
+			int value = stack.pop();
+			System.out.println("栈顶元素：" + value);
+		}
+	}
+}
